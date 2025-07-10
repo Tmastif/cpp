66f0d14fdf5534e94d1434e72fb4224b495236f7
Inbar Lazar <ilazar@c3a7c2.42berlin.de>
2025-07-10 18:27:19 +0200

    09

--- 09/ex02/PmergeMe.hpp
+++ 09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ilazar <ilazar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 10:59:31 by ilazar            #+#    #+#             */
-/*   Updated: 2025/07/08 15:35:46 by ilazar           ###   ########.fr       */
+/*   Updated: 2025/07/10 18:09:50 by ilazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,15 @@ class PmergeMe
     public:
         bool    parse_input(int ac, char **av);
         
-        std::vector<int>    fj_sort(std::vector<int> seq);
-        std::deque<int>     fj_sort(std::deque<int> seq);
         int    action(int ac, char **av);
+        
+        std::vector<int>    fj_sort(std::vector<int> &seq, int &comparisments);
+        std::deque<int>     fj_sort(std::deque<int> seq);
 
+        void binary_insert(std::vector<int> &vec, int val, int &comp);
+        void  insert_pend_jacobsthal(std::vector<int>& main, const std::vector<int>& pend, int &total_comparisons);
+        std::vector<int> jacobsthal_numbers(int max_n);
+        std::vector<int> jacobsthal_insertion_order(int m);
         
         //Helpers
         void    print(const std::vector<int> &a) const;
